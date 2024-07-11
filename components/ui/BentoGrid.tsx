d14b792acc1ffb994f28f158dc9c59c25107b814
BISHAL DATTA <dattabishal001@gmail.com>
2024-07-11 12:08:04 +0530

    feat: 3d  Globe added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,12 @@
+'use client'
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GrideGlobe";
+import { useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "@/data/confetti.json";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
     className,
@@ -39,10 +46,15 @@ export const BentoGridItem = ({
     titleClassName?: string;
     spareImg?: string;
 }) => {
+    const [copied, setCopied] = useState(false);
+    const handleCopy=()=>{
+        navigator.clipboard.writeText('dattabishal001@gmail.com')
+        setCopied(true);
+    }
     return (
         <div
             className={cn(
-                "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+                "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
                 className
             )}
             style={{
@@ -51,7 +63,7 @@ export const BentoGridItem = ({
                     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
             }}
         >
-            <div className={`${id === 6 && 'flex justify-center h-full'} `}>
+            <div className={`${id === 6 && 'flex justify-center'} h-full`}>
                 <div className="w-full h-full absolute">
                     {
                         img && (
@@ -82,13 +94,76 @@ export const BentoGridItem = ({
                     )
                 }
                 <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full flex flex-col px-5 p-5 ld:p-10')}>
-                <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
-                    {description}
+                    <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+                        {description}
+                    </div>
+                    <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10 ">
+                        {title}
+                    </div>
+                    {
+                        id === 2 && <GlobeDemo />
+                    }
+                    {id === 3 && (
+                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                            {/* tech stack lists */}
+                            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                                {['React.js', 'Next.js', 'TypeScript'].map((item, i) => (
+                                    <span
+                                        key={i}
+                                        className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                                    >
+                                        {item}
+                                    </span>
+                                ))}
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                            </div>
+                            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                                {['Firebase', 'Material-ui', 'version-Controll', 'Javascript'].map((item, i) => (
+                                    <span
+                                        key={i}
+                                        className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                                    >
+                                        {item}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                    {id === 6 && (
+                        <div className="mt-5 relative">
+                            {/* button border magic from tailwind css buttons  */}
+                            {/* add rounded-md h-8 md:h-8, remove rounded-full */}
+                            {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+                            {/* add handleCopy() for the copy the text */}
+                            <div
+                                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
+                                    }`}
+                            >
+                                {/* <img src="/confetti.gif" alt="confetti" /> */}
+                                <Lottie options={{
+                                    loop: copied,
+                                    autoplay: copied,
+                                    animationData: animationData,
+                                    rendererSettings: {
+                                        preserveAspectRatio: "xMidYMid slice",
+                                    },
+                                }} height={200} width={400} />
+                            </div>
+
+                            <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
+                        </div>
+                    )}
                 </div>
-                <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10 ">
-                    {title}
-                </div>
-                </div>
+
             </div>
         </div>
     );
